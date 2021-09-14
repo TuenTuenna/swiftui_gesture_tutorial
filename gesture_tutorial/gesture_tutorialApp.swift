b082f9593732a8791acc846f783ba46ce4b16805
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-09-14 20:51:14 +0900

    탭 제스쳐

---
+++ gesture_tutorial/gesture_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  gesture_tutorialApp.swift
+//  gesture_tutorial
+//
+//  Created by Jeff Jeong on 2021/09/14.
+//
+
+import SwiftUI
+
+@main
+struct gesture_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
